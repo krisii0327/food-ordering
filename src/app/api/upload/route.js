4d7c2643f0d4c9f985c0bae311df9cb5d0577da8
Kristof Kovesi <kovesi44@gmail.com>
2023-12-15 21:06:44 +0100

    remove console.logs

--- src/app/api/upload/route.js
+++ src/app/api/upload/route.js
@@ -5,7 +5,6 @@ export async function POST(req) {
     const data = await req.formData();
 
     if(data.get('file')) {
-        //console.log('we have file', data.get('file'));
         const file = data.get('file');
 
         const s3Client = new S3Client({
