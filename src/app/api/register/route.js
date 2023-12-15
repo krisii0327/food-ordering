686f10962e712a1ecd9b3d7ed06bd47941f01dd6
Kristof Kovesi <kovesi44@gmail.com>
2023-12-15 20:52:10 +0100

    fix mongodb

--- src/app/api/register/route.js
+++ src/app/api/register/route.js
@@ -6,7 +6,7 @@ export async function POST(req) {
     const body = await req.json();
     
     mongoose.connect(process.env.MONGO_URL);
-    //mongoose.connect('mongodb+srv://kovesi44:xXn3rJrX9pRn8G03@cluster0.0pcwz2u.mongodb.net/food-ordering-app?retryWrites=true&w=majority');
+
     const pass = body.password;
     if(!pass?.length  || pass.length < 5) {
         new Error('password must be at least 5 characters');
